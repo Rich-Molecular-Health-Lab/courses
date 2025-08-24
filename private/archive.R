564fb5de9314dc64c0531388190919ca424ef38e
aliciamrich <aliciamrich@gmail.com>
2025-08-24 12:13:29 -0500

    nojekyll

---
+++ private/archive.R
@@ -0,0 +1,11 @@
+## Current Courses (Fall 2025)
+
+- **Conservation Biology** *(BIOL-4120/BIOL-8126)*
+  - [Syllabus](syllabus_conbio.qmd)
+- [Schedule](schedule_conbio.qmd)
+
+
+- **Human Health & the Environment** *(ENVN-4320/BIOL-4030)*
+  - [Syllabus](syllabus_hhe.qmd)
+- [Schedule](schedule_hhe.qmd)
+
