124e0cd78b80d0b87a39c4c0ab668d15284f1a40
aliciamrich <aliciamrich@gmail.com>
2025-08-29 13:56:19 -0500

    update check

--- styles/palettes.R
+++ styles/palettes.R
@@ -1,9 +1,103 @@
-butterfly <- c("#784116FF", "#9CB6A9FF", "#ACCFCBFF", "#A8BE74FF", "#BF7417FF")
-navajo    <- c("#660D20FF", "#E59A52FF", "#EDCE79FF", "#094568FF", "#E1C59AFF")
-acadia    <- c("#FED789FF", "#023743FF", "#72874EFF", "#476F84FF", "#A4BED5FF", "#453947FF")
-arches    <- c("#A8CDECFF", "#F6955EFF", "#682C37FF", "#9B6981FF", "#7887A4FF", "#A89F8EFF")
-saguaro   <- c("#847CA3FF", "#E45A5AFF", "#F4A65EFF", "#80792BFF", "#F2D56FFF", "#1A1237FF")
 
-derain    <- c("#EFC86EFF", "#97C684FF", "#6F9969FF", "#AAB5D5FF", "#808FE1FF", "#5C66A8FF", "#454A74FF")
-monet     <- c("#4E6D58FF", "#749E89FF", "#ABCCBEFF", "#E3CACFFF", "#C399A2FF", "#9F6E71FF", "#41507BFF", "#7D87B2FF", "#C2CAE3FF")
-shuksan2  <- c("#5D74A5FF", "#B0CBE7FF", "#FEF7C7FF", "#EBA07EFF", "#A8554EFF")
+butterfly <- c(
+  "#784116FF",
+  "#9CB6A9FF",
+  "#ACCFCBFF",
+  "#A8BE74FF",
+  "#BF7417FF")
+
+navajo    <- c(
+  "#660D20FF",
+  "#E59A52FF",
+  "#EDCE79FF",
+  "#094568FF",
+  "#E1C59AFF")
+
+acadia    <- c(
+  "#FED789FF",
+  "#023743FF",
+  "#72874EFF",
+  "#476F84FF",
+  "#A4BED5FF",
+  "#453947FF")
+
+arches    <- c(
+  "#A8CDECFF",
+  "#F6955EFF",
+  "#682C37FF",
+  "#9B6981FF",
+  "#7887A4FF",
+  "#A89F8EFF")
+
+saguaro   <- c(
+  "#847CA3FF",
+  "#E45A5AFF",
+  "#F4A65EFF",
+  "#80792BFF",
+  "#F2D56FFF",
+  "#1A1237FF")
+
+derain    <- c(
+  "#EFC86EFF",
+  "#97C684FF",
+  "#6F9969FF",
+  "#AAB5D5FF",
+  "#808FE1FF",
+  "#5C66A8FF",
+  "#454A74FF")
+
+monet     <- c(
+  "#4E6D58FF",
+  "#749E89FF",
+  "#ABCCBEFF",
+  "#E3CACFFF",
+  "#C399A2FF",
+  "#9F6E71FF",
+  "#41507BFF",
+  "#7D87B2FF",
+  "#C2CAE3FF")
+
+shuksan2  <- c(
+  "#5D74A5FF",
+  "#B0CBE7FF",
+  "#FEF7C7FF",
+  "#EBA07EFF",
+  "#A8554EFF")
+
+taylor1989      <- c(
+  "#B1532AFF",
+  "#84697FFF",
+  "#CBB593FF",
+  "#A88F92FF",
+  "#E8EADFFF",
+  "#43475BFF")
+
+taylormidnights <- c(
+  "#586891FF",
+  "#8897A4FF",
+  "#B3A6A3FF",
+  "#2B152CFF",
+  "#F1F3F2FF")
+
+warhol_camo_87_5       <- c(
+  "#6F7434FF",
+  "#D0A858FF",
+  "#F9FCDFFF",
+  "#BFCFE3FF")
+
+wesanderson_Chevalier1 <- c(
+  "#446455FF",
+  "#FDD262FF",
+  "#D3DDDCFF",
+  "#C7B19CFF")
+
+JoanMiro               <- c(
+  "#C04759FF",
+  "#3B6C73FF",
+  "#383431FF",
+  "#F1D87FFF",
+  "#EDE5D2FF")
+
+
+
+
