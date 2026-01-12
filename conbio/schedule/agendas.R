aa99c8b6535fe9261b9006808a2b0bc22ae37ede
aliciamrich <aliciamrich@gmail.com>
2026-01-12 15:00:05 -0600

    conbio syllabus lecture done

--- conbio/schedule/agendas.R
+++ conbio/schedule/agendas.R
@@ -3,24 +3,28 @@ agendas <- list(
     D1 = list(
       sections = c(NA),
       topic    = "Overview & Syllabus",
-      agenda   = NA
+      agenda   = NA,
+      materials = "slides/conbio/0_conbio_syllabus_overview.html"
     ),
     D2 = list(
       sections = c(2.2),
       topic    = "The Rise of Conservation Biology",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
       )
   ),
   W2 = list(
     D1 = list(
       sections = c(NA),
       topic    = "Holiday",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(3.2, 3.3, 3.4, 3.5, 3.6),
       topic    = "Biodiversity Concepts & Measurement",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W3 = list(
@@ -32,170 +36,198 @@ agendas <- list(
     D2 = list(
       sections = c(3.2, 3.3, 3.4, 3.5, 3.6),
       topic    = "Biodiversity Concepts & Measurement",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W4 = list(
     D1 = list(
       sections = c(8.1, 8.3),
       topic    = "Extinction",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
       ),
     D2 = list(
       sections = c(8.1, 8.3),
       topic    = "Extinction",
-      agenda   = "Case Conversation 1"
+      agenda   = "Case Conversation 1",
+      materials = NA
     )
   ),
   W5 = list(
     D1 = list(
       sections = c(9.1, 9.2, 9.3, 9.4),
       topic    = "Habitat Loss, Fragmentation, and Degradation",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(9.1, 9.2, 9.3, 9.4),
       topic    = "Habitat Loss, Fragmentation, and Degradation",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W6 = list(
     D1 = list(
       sections = c(9.1, 9.2, 9.3, 9.4),
       topic    = "Habitat Loss, Fragmentation, and Degradation",
-      agenda   = "Case Conversation 2"
+      agenda   = "Case Conversation 2",
+      materials = NA
     ),
     D2 = list(
       sections = c(10.1, 10.2, 10.3, 10.4),
       topic    = "Overexploitation",
-      agenda   = "Fishing Game"
+      agenda   = "Fishing Game",
+      materials = NA
     )
   ),
   W7 = list(
     D1 = list(
       sections = c(10.1, 10.2, 10.3, 10.4),
       topic    = "Overexploitation",
-      agenda   = "Case Conversation 3"
+      agenda   = "Case Conversation 3",
+      materials = NA
     ),
     D2 = list(
       sections = c(11.1, 11.2, 11.4),
       topic    = "Invasive Alien Species",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W8 = list(
     D1 = list(
       sections = c(NA),
       topic    = NA,
-      agenda   = "Exam 1"
+      agenda   = "Exam 1",
+      materials = NA
     ),
     D2 = list(
       sections = c(11.1, 11.2, 11.4),
       topic    = "Invasive Alien Species",
-      agenda   = "Case Conversation 4"
+      agenda   = "Case Conversation 4",
+      materials = NA
     )
   ),
   W9 = list(
     D1 = list(
       sections = c(12.1, 12.2, 12.3, 12.4),
       topic    = "Climate Change",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(12.1, 12.2, 12.3, 12.4),
       topic    = "Climate Change",
-      agenda   = "Case Conversation 5"
+      agenda   = "Case Conversation 5",
+      materials = NA
     )
   ),
   W10 = list(
     D1 = list(
       sections = c(NA),
       topic    = "Spring Break",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(NA),
       topic    = "Spring Break",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W11 = list(
     D1 = list(
       sections = c(16.1, 16.2),
       topic    = "Ex Situ Conservation",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(16.1, 16.2),
       topic    = "Ex Situ Conservation",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W12 = list(
     D1 = list(
       sections = c(13.1, 13.2, 13.3, 13.4, 13.5, 13.6),
       topic    = "Species-Level Approaches",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(13.1, 13.2, 13.3, 13.4, 13.5, 13.6),
       topic    = "Species-Level Approaches",
-      agenda   = "Case Conversation 6"
+      agenda   = "Case Conversation 6",
+      materials = NA
     )
   ),
   W13 = list(
     D1 = list(
       sections = c(13.1, 13.2, 13.3, 13.4, 13.5, 13.6),
       topic    = "Species-Level Approaches",
-      agenda   = "Population Management Excercise"
+      agenda   = "Population Management Excercise",
+      materials = NA
     ),
     D2 = list(
       sections = c(14.1, 14.2, 14.4),
       topic    = "Community & Ecosystem Approaches",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   W14 = list(
     D1 = list(
       sections = c(14.1, 14.2, 14.4),
       topic    = "Community & Ecosystem Approaches",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(14.1, 14.2, 14.4),
       topic    = "Community & Ecosystem Approaches",
-      agenda   = "Case Conversation 7"
+      agenda   = "Case Conversation 7",
+      materials = NA
     )
   ),
   W15 = list(
     D1 = list(
       sections = c(15.3, 15.4),
       topic    = "Landscape-Scale Approaches",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(15.3, 15.4),
       topic    = "Landscape-Scale Approaches",
-      agenda   = "Case Conversation 8"
+      agenda   = "Case Conversation 8",
+      materials = NA
     )
   ),
   W16 = list(
     D1 = list(
       sections = c(NA),
       topic    = "Review",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     ),
     D2 = list(
       sections = c(NA),
       topic    = "Review",
-      agenda   = NA
+      agenda   = NA,
+      materials = NA
     )
   ),
   Finals = list(
     D1 = list(
       sections = c(NA),
       topic    = NA,
-      agenda   = "Final Exam"
+      agenda   = "Final Exam",
+      materials = NA
     )
   )
 )  %>%
@@ -211,5 +243,6 @@ agendas <- list(
     Day,
     reading,
     topic,
-    agenda
+    agenda,
+    materials
   )
